2b4d3d12e04f7f50b1f0304b3eff0b22d47b7d21
Timofey Agapitov <57286657+Vatai1@users.noreply.github.com>
2022-11-01 16:56:30 +1100

    Update install-server.sh

--- install-server.sh
+++ install-server.sh
@@ -81,14 +81,9 @@ openssl x509 -req -days 3650 -in /etc/openvpn/mikrotik-ssl/client-${PORT}.crt \
     -set_serial 01 -out /etc/openvpn/mikrotik-ssl/client-${PORT}.crt
 
 
-while true;
-do
-        SUB1=2
-        SUB2=2
-        if [[ $(ip route | grep "10.${SUB1}.${SUB2}.0/24" | wc -l) == "0" ]]; then
-                break;
-        fi
-done
+SUB1=2
+SUB2=2
+  
 
 cat <<EOF > ${OPENVPN_CONFIG_DIR}/server${PORT}.conf
 daemon
